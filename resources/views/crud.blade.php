8bb92fa0ce627be3367297ff711327f4f5d98ac5
binh123440 <binhgolden123@gmail.com>
2024-11-25 13:32:20 +0700

    improve: better pagination in manage podcast

--- resources/views/crud.blade.php
+++ resources/views/crud.blade.php
@@ -149,23 +149,13 @@
                       </div>
                       @endforeach
 
-                      {{ $podcasts->links() }}
+          
 
                     </div>
                     <div class="container" data-aos="fade-up">
                       <div class="row">
                         <div class="col-md-12 text-center">
-                          <div class="site-block-27">
-                            <ul>
-                              <li><a href="#" class="icon-keyboard_arrow_left"></a></li>
-                              <li class="active"><span>1</span></li>
-                              <li><a href="#">2</a></li>
-                              <li><a href="#">3</a></li>
-                              <li><a href="#">4</a></li>
-                              <li><a href="#">5</a></li>
-                              <li><a href="#" class="icon-keyboard_arrow_right"></a></li>
-                            </ul>
-                          </div>
+                            {{ $podcasts->links('pagination::bootstrap-4') }}
                         </div>
                       </div>
                     </div>
